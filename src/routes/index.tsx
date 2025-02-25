7635e5a7f6f22c924749d48c09cbe135e76cc4b1
heartfeltfy <15239905718@163.com>
2025-02-25 13:58:54 +0800

    404

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -28,7 +28,11 @@ const router = createBrowserRouter([
       },
       {
         path: "*",
-        element: <NotFound />,
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <NotFound />
+          </Suspense>
+        ),
       },
     ],
   },
