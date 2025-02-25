cd5c07418ea0023377c86a411b6b72852c5438ab
heartfeltfy <15239905718@163.com>
2025-02-25 13:43:11 +0800

    路由404

---
+++ docs/assets/NotFound-Cqcu6a3o.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B8N1K6Lz.js";const r=()=>o.jsx("div",{children:"NotFound"});export{r as default};
---
+++ docs/assets/index-CZFGT2Gc.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-B8N1K6Lz.js";const r=()=>t.jsx("div",{children:"About"});export{r as default};
