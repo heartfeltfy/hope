214c97585eba7fc377bc2ae662ef7c1dfb3e897b
heartfeltfy <15239905718@163.com>
2025-02-26 22:50:57 +0800

    明天总会是好的

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,24 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-br from-slate-100 to-blue-100 text-slate-700 font-sans">
+      <h1 className="text-4xl mb-6 font-bold drop-shadow-lg">
+        愿我所有的努力都不被辜负
+      </h1>
+      <div className="text-lg leading-loose text-center max-w-2xl px-4">
+        <p>
+          愿我在未来的日子里，
+          <br />
+          心怀勇气，充满希望，
+          <br />
+          遇见最好的自己。
+          <br />
+          愿所有的美好如约而至，
+          <br />
+          所有的付出都能开花结果。
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
