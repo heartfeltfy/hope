8685baf8463c2bf51f1a3843e855bd795ead2359
heartfeltfy <15239905718@163.com>
2025-06-18 11:46:41 +0800

    新增sonner、sweetalert2

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { Provider } from 'mobx-react'
 import { stores } from '@/stores'
 import './index.css'
 import App from './App'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={stores}>
+      <Toaster />
       <App />
     </Provider>
   </StrictMode>
